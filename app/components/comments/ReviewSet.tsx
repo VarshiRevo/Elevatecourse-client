06c2bd76f26332755c41c1504473be9a46caba36
Varshiga P S <145317472+VarshiRevo@users.noreply.github.com>
2024-12-26 13:16:00 +0530

    Update ReviewSet.tsx

--- app/components/comments/ReviewSet.tsx
+++ app/components/comments/ReviewSet.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ReviewSetProps } from '..//../types';
+import { ReviewSetProps } from './types';
 import { ReviewCard } from './ReviewCard';
 
 export const ReviewSet: React.FC<ReviewSetProps> = ({ reviews }) => {
